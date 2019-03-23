5129e3c1ff11d811f94f45f405c48ce699c0b603
Jonan <jonanhie24@gmail.com>
2019-03-23 17:22:07 +0100

    Falta el plot

--- cap_10/ej5.r
+++ cap_10/ej5.r
@@ -4,18 +4,20 @@
 x <- -5:5; 
 
 # 1 valor para x
-x <- -5;
+x <- -3;
 
 # Definicion de f(x)
-f <- function(x){x^2}
- if(x<0){
- f <- function(x){sin(x)}
+
+if(x<0){
+	f <- function(x){sin(x)}
 }else if (x>=2){
- f <- function(x){4*exp(1)^(x-2)}
+	f <- function(x){4*exp(1)^(x-2)}
+}else{
+	f <- function(x){x^2}
 }
 
-# Plot de f(x)
-plot(f,5,-5,type="l")
+# Plot de f(x) FALTA QUE LO HAGA 
+#plot(f,5,-5,type="l")
 
 #Resultado de f(x)
 paste("El valor de f es", f(x))
