d0bb5eb5141aac0f34a665199bb04358f49e92b3
Ander Raso Vázquez <anderraso@gmail.com>
2019-04-06 19:12:47 +0200

    modificado ej4
    Signed-off-by: Ander Raso Vázquez <anderraso@gmail.com>

--- cap_11/ej4.r
+++ cap_11/ej4.r
@@ -1,17 +1,15 @@
 library(MASS)
 data(cabbages)
 attach(cabbages)
-
 boxplot(HeadWt~Cult, main="HeadWt by Cult")
 boxplot(HeadWt~Date, main="HeadWt by Date")
 
 
 summary(VitC)
-median(VitC)
 var(VitC)
 sd(VitC)
 
 regresion <- lm(VitC~HeadWt)
-plot(VitC~HeadWt)
-abline(lm(VitC~HeadWt))
+plot(VitC~HeadWt, main="Regresión lineal")
+abline(regresion)
 predict(regresion, data.frame(HeadWt = c(2.125)))
